c7e890fc24fea930cde6b8507ee8ee885643f30c
kenomun <eugenio.mun.f@gmail.com>
2024-01-15 11:32:13 -0300

    primer comit creacion de proyecto

---
+++ src/test/java/com/emunoz/inversiones/billetera/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emunoz.inversiones.billetera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
