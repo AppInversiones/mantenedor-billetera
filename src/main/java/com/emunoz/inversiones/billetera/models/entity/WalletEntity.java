3783f3ae3f5416462b70b8d1bb9b8b2dd4caa11e
kenomun <eugenio.mun.f@gmail.com>
2024-01-15 16:31:51 -0300

    modelos entity, request, response y configuracion de base de datos

--- src/main/java/com/emunoz/inversiones/billetera/models/entity/WalletEntity.java
+++ src/main/java/com/emunoz/inversiones/billetera/models/entity/WalletEntity.java
@@ -1,7 +1,35 @@
 package com.emunoz.inversiones.billetera.models.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wallets")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class WalletEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "usd_balance")
+    private Float usd_balance;
+
+    @Column(name = "date_update")
+    private LocalDateTime date_update;
+
+    @Column(name = "user_id")
+    private Long user_id;
 
 
 }
--- src/main/java/com/emunoz/inversiones/billetera/models/response/WalletRespnseDTo.java
+++ src/main/java/com/emunoz/inversiones/billetera/models/response/WalletRespnseDTo.java
@@ -1,4 +1,14 @@
 package com.emunoz.inversiones.billetera.models.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class WalletRespnseDTo {
+
+    private String message;
+    private Object data;
+    private Integer code;
+
 }
